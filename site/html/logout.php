4cf47def37c4ec3f331a998546c25bb85ccbb7cf
csemblat <csemblat@protonmail.com>
2020-10-11 21:10:40 +0200

    added index for root of server

--- site/html/logout.php
+++ site/html/logout.php
@@ -1,10 +1,8 @@
 <?php
 session_start();
-if(isset($_SESSION['id'])){ //logs out users that were already logged in
-    $_SESSION["role"] = NULL;
-    $_SESSION["id"] = NULL;
-    $_SESSION["user"] = NULL;
-}
+$_SESSION["role"] = NULL;
+$_SESSION["id"] = NULL;
+$_SESSION["user"] = NULL;
 header("Location: ./login.php");
 die();
 ?>
